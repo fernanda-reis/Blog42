13987c0b5c2dd66e1c54a8ba1a64ac0c22930d4d
Fernanda <fernandasoares.reis1@gmail.com>
2022-02-23 17:13:39 -0300

    componente início sem consumo

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { UserLogin } from '../model/UserLogin';
 import { Usuario } from '../model/Usuario';
 
@@ -17,5 +18,15 @@ export class AuthService {
 
   cadastrar(usuario: Usuario): Observable<Usuario>{
     return this.http.post<Usuario>('https://blog-pessoal-42.herokuapp.com/usuarios/cadastrar', usuario)
+  }
+
+  logado(){
+    let ok = false
+
+    if(environment.token != ''){
+      ok = true
+    }
+
+    return ok
   }
 }
